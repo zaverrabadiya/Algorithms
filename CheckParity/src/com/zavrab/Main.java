06035427dccced7d224650c86e59d4dff5d557c3
Zaver Rabadiya <zavrab@microsoft.com>
2016-02-08 12:48:27 -0800

    Check parity program

---
+++ CheckParity/src/com/zavrab/Main.java
@@ -0,0 +1,23 @@
+package com.zavrab;
+/*
+ * @author Zaver R
+ * Date: Feb 8, 2016
+*/
+public class Main {
+
+    public static void main(String[] args) {
+        //Count number of 1's and return 1 if its odd else 0
+        //Check parity of 1011 (11) should return 1
+        //10100 (20) should return 0
+        System.out.println("Parity is: " + checkParity(11));
+    }
+
+    public static short checkParity(long x) {
+        short result = 0;
+        while (x != 0) {
+            result ^= 1;
+            x &= x - 1;
+        }
+        return result;
+    }
+}
