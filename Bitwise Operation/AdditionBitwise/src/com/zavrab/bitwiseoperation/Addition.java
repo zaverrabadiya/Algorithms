2548dc07c1dd91d967878f5e7b4f35fb4af7c090
Zaver Rabadiya <zavrab@microsoft.com>
2016-02-12 16:54:06 -0800

    Addition using bitwise operation

---
+++ Bitwise Operation/AdditionBitwise/src/com/zavrab/bitwiseoperation/Addition.java
@@ -0,0 +1,23 @@
+package com.zavrab.bitwiseoperation;
+
+/**
+ * Created by Zaver R on 2/12/16.
+ */
+public class Addition {
+
+    public static long sum(long x, long y) {
+        long tempX = x, tempY = y, carryIn = 0, sum = 0, k = 1;
+        while(tempX != 0 || tempY != 0) {
+            long xk = (x & k), yk = (y & k);
+            long carryOut = (xk & yk) | (xk & carryIn) | (yk & carryIn);
+            sum |= xk ^ yk ^ carryIn;
+
+            carryIn = carryOut << 1;
+            k <<= 1;
+            tempX >>= 1;
+            tempY >>= 1;
+        }
+        sum = sum | carryIn;
+        return sum;
+    }
+}
---
+++ Bitwise Operation/AdditionBitwise/src/com/zavrab/bitwiseoperation/Main.java
@@ -0,0 +1,9 @@
+package com.zavrab.bitwiseoperation;
+
+public class Main {
+
+    public static void main(String[] args) {
+        long x = 11, y = 5;
+        System.out.println("Sum of " + x + "+" + y + " = " + Addition.sum(x, y));
+    }
+}
