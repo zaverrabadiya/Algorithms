1cc1a6e2dd76e8452fcf6dbd6046820008cce0d5
Zaver Rabadiya <zavrab@microsoft.com>
2016-02-15 22:09:07 -0800

    Reverse digits

---
+++ Bitwise Operation/ReverseDigits/src/com/zavrab/bitwiseoperation/Main.java
@@ -0,0 +1,12 @@
+package com.zavrab.bitwiseoperation;
+
+/**
+ * Created by Zaver R on 2/15/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Reverse digits: " + ReverseDigits.reverse(54));
+    }
+}
---
+++ Bitwise Operation/ReverseDigits/src/com/zavrab/bitwiseoperation/ReverseDigits.java
@@ -0,0 +1,16 @@
+package com.zavrab.bitwiseoperation;
+
+/**
+ * Created by Zaver R on 2/15/16.
+ */
+public class ReverseDigits {
+    public static long reverse(long x) {
+        boolean isNegative = x < 0;
+        long result = 0;
+        while (x != 0) {
+            result = (result * 10) + (x % 10);
+            x /= 10;
+        }
+        return (isNegative)? -result : result;
+    }
+}
