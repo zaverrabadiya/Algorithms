cd8c4925cac55eb85e189ea784680b18647c0941
Zaver Rabadiya <zavrab@microsoft.com>
2016-02-17 00:25:22 -0800

    Rearrange array based on given pivot

---
+++ ArrayProblems/RearrangeToPivot/src/com/zavrab/bitwiseoperation/Main.java
@@ -0,0 +1,45 @@
+package com.zavrab.bitwiseoperation;
+
+/*
+* Created by Zaver R on 02/17/2016
+* */
+public class Main {
+
+    public static void main(String[] args) {
+        int[] a = new int[] {9, 2, 3, 6, 5, 7, 4, 8, 6};
+
+        String input = "";
+        for(int v : a) {
+            input += v + " ";
+        }
+        System.out.println("Input : " + input);
+
+        rearrange(a, 3);
+        String output = "";
+        for(int v : a) {
+            output += v + " ";
+        }
+        System.out.println("Output : " + output);
+    }
+
+    public static void rearrange(int[] A, int pivotIndex) {
+        int i =0, j = 0, l = A.length;
+        int pivot = A[pivotIndex];
+        while(j < l) {
+            if(A[j] < pivot) {
+                swap(A, i++, j++);
+
+            } else if(A[j] == pivot) {
+                ++j;
+            } else {
+                swap(A, j, --l);
+            }
+        }
+    }
+
+    private static void swap(int[] A, int s, int d) {
+        int temp = A[d];
+        A[d] = A[s];
+        A[s] = temp;
+    }
+}
