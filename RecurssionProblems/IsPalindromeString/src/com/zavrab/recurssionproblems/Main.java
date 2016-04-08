58bab441f41208e588f8eda95f4b77545df04f20
Zaver Rabadiya <zavrab@microsoft.com>
2016-04-08 14:03:56 -0700

    Adding test is palindrome string recursively

---
+++ RecurssionProblems/IsPalindromeString/src/com/zavrab/recurssionproblems/Main.java
@@ -0,0 +1,49 @@
+package com.zavrab.recurssionproblems;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //e.g.
+        //"racecar" = true
+        //"-racecar" = true
+        //"racecar!" = true
+        //"RaceCar" = true
+        //"Race car" = true
+        //"test" = false
+        //"a" = true
+
+        String s = "racecar";
+        System.out.println("Is Palindrome: " + isPalindrome(s));
+    }
+
+
+    static boolean isPalindrome(String strInput) {
+        if (strInput.length() <= 1) {
+            return true;
+        }
+
+        String first = strInput.substring(0, 1);
+        String last = strInput.substring(strInput.length() - 1);
+        if (first.equalsIgnoreCase(last)) {
+            return isPalindrome(strInput.substring(1, strInput.length() - 1));
+        }
+
+        if (ignore(first.charAt(0))) {
+            return isPalindrome(strInput.substring(1));
+        } else if (ignore(last.charAt(0))) {
+            return isPalindrome(strInput.substring(0, strInput.length() -1));
+        } else {
+            return false;
+        }
+    }
+
+    static boolean ignore(char c) {
+        return (isPunctuation(c) || c == ' ');
+    }
+
+    static boolean isPunctuation(char c) {
+        return c == '.' || c == ',' || c == '?' || c == '!' || c =='\'' || c == ';' || c == ':' || c == '"';
+    }
+
+}
