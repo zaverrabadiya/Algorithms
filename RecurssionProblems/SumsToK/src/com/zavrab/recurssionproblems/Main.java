63eabf63b9ef02dff33f869d575af3dc3736a5f5
Zaver Rabadiya <zavrab@microsoft.com>
2016-04-18 17:44:21 -0700

    sumstok and nearestNNeighbor

---
+++ RecurssionProblems/SumsToK/src/com/zavrab/recurssionproblems/Main.java
@@ -0,0 +1,36 @@
+package com.zavrab.recurssionproblems;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Given an array of integers, find all TRIPLETS that sum to a given integer 0
+        // e.g.
+        // Input = {2, 4, 8}, 6 => true
+        // Input = { 2, -4, 8 }, 1 => false
+        // Input = {2, 4, 8}, 2 => true
+
+        int[] input = {2, 4, 8};
+
+        System.out.print("Result: " + isSumK(input, 14));
+    }
+
+    public static boolean isSumK(int[] inArray, int k) {
+
+        return isSumK(inArray, 0, k);
+    }
+
+    private static boolean isSumK(int[] arr, int s, int sum) {
+
+        if (sum == 0) {
+            return true;
+        }
+
+        if (s == arr.length) {
+            return false;
+        }
+
+        return isSumK(arr, s + 1, sum) ||
+                isSumK(arr, s + 1, sum - arr[s]);
+
+    }
+}
---
+++ SortingProblems/NearestNNeighbor/src/com/zavrab/sortingproblems/Main.java
@@ -0,0 +1,87 @@
+package com.zavrab.sortingproblems;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Point point = new Main().new Point(3, 3);
+        Point[] nearestPoints = findNearestPoints(generateAllPoints(), point, 2);
+
+        for (Point p : nearestPoints) {
+            System.out.println("{" + p.x + ", " + p.y + "}" );
+        }
+    }
+
+    public static Point[] findNearestPoints(Point[] allPoints, Point point, int kPoint) {
+        PriorityQueue<Point> queueByDistance = new PriorityQueue<Point>(kPoint, Collections.reverseOrder());
+
+        for (int i = 0; i < allPoints.length; i++) {
+            double distance = euclideanDistance(point, allPoints[i]);
+            allPoints[i].setDistance(distance);
+            queueByDistance.add(allPoints[i]);
+
+            if (queueByDistance.size() > kPoint) {
+                queueByDistance.poll();
+            }
+        }
+        return getTopKPointsFromPQ(kPoint, queueByDistance);
+    }
+
+    private static double euclideanDistance(Point s, Point d) {
+        double xSqr = Math.pow((s.x - d.x), 2);
+        double ySqr = Math.pow((s.y - d.y), 2);
+
+        return Math.sqrt(xSqr + ySqr);
+    }
+
+    private static Point[] getTopKPointsFromPQ(int kPoint, PriorityQueue<Point> allPoints) {
+        Point[] nearestPoints = new Point[kPoint];
+        int i = kPoint - 1;
+        while (allPoints.size() > 0) {
+            nearestPoints[i--] = allPoints.poll();
+        }
+        return nearestPoints;
+    }
+
+    private static Point[] generateAllPoints() {
+        Point[] nPoints = new Point[5];
+        nPoints[0] = new Main().new Point(0, 6);
+        nPoints[1] = new Main().new Point(6, 0);
+        nPoints[2] = new Main().new Point(3, 2);
+        nPoints[3] = new Main().new Point(3, 6);
+        nPoints[4] = new Main().new Point(5, 3);
+
+        return nPoints;
+    }
+
+    private class Point implements Comparable {
+        int x;
+        int y;
+        double distance;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        void setDistance(double distance) {
+            this.distance = distance;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            Point point = (Point) o;
+
+            if (this.distance < point.distance) {
+                return -1;
+            } else if (this.distance > point.distance) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+}
