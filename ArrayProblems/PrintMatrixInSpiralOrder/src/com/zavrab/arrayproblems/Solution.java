5ebc6befb567ebfd97a929996ec4278f37113f03
Zaver Rabadiya <zavrab@microsoft.com>
2016-05-09 11:41:29 -0700

    Adding print matrix spirally

---
+++ ArrayProblems/PrintMatrixInSpiralOrder/src/com/zavrab/arrayproblems/Solution.java
@@ -0,0 +1,51 @@
+package com.zavrab.arrayproblems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int n = 4, m = 4;
+        int[][] matrix = new int[n][m];
+        int val = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = val++;
+            }
+        }
+
+        printMatrixInSpiral(matrix, n, m);
+    }
+
+    public static List<Integer> printMatrixInSpiral(int[][] matrix, int n, int m) {
+        int border = 0, i, j;
+        List<Integer> result = new ArrayList<Integer>();
+
+        while (border < (n - border) || border < (m - border)) {
+            i = border;
+            j = border;
+
+            for (;j < (m - border); j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            j--; // Decrease since j went out of bound
+
+            for (i = i + 1; i < (n - border); i++) { // Start from the next row
+                System.out.print(matrix[i][j] + " ");
+            }
+            i--; //Decrease since i went out of bound
+
+            for (j = j - 1; j > border; j--) { // Start from prev col
+                System.out.print(matrix[i][j] + " ");
+            }
+
+            for (; i > border; i--) {
+                System.out.print(matrix[i][j] + " ");
+            }
+
+            border++;
+        }
+        return result;
+    }
+}
