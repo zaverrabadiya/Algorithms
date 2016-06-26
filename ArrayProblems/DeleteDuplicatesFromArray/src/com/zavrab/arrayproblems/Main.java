51a63c0b23f34ac520c1694528de7e50aae7b50e
Zaver Rabadiya <zavrab@microsoft.com>
2016-06-26 10:40:50 -0700

    Adding find missing number in 4 billion integers solution

--- ArrayProblems/DeleteDuplicatesFromArray/src/com/zavrab/arrayproblems/Main.java
+++ ArrayProblems/DeleteDuplicatesFromArray/src/com/zavrab/arrayproblems/Main.java
@@ -1,20 +1,15 @@
 package com.zavrab.arrayproblems;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.BitSet;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-
-
         int[] sorted = new int[] {2,3, 5, 5, 7, 11, 11, 11, 13};
         //int[] result = deleteDuplicatesFromSorted(sorted);
 
-        int[] unsorted = new int[] {88, 4, 2, 3, 21, 17, 2, 3, 17, 21, 88, 17, 17};
+        int[] unsorted = new int[] {88, 4, 2, 3, 21, 17, 2, 3, 8, 17, 21, 88, 17, 17, 1000000};
 
         int[] result = deleteDuplicatesFromUnSorted(unsorted);
 
@@ -22,6 +17,41 @@ public class Main {
         for (int i : result) {
             System.out.print(i + " ");
         }
+    }
+
+    // Removes duplicates from UNSORTED array,
+    // Array can have billions of elements
+    // Using byte array to set bit corresponds to the value in original array
+    // BitSet table would look like: b represents byte[]
+    // 4 => b[0]: 0 0 0 1 0 0 0
+    // 3 => b[0]: 0 0 1 0 0 0 0
+    // 15 => b[1]: 0 0 0 0 0 1 0
+    // 21 => b[2]: 0 0 0 0 1 0 0
+    // 88 => b[11]: 0 0 0 0 0 0 1
+    public static int[] deleteDuplicatesFromUnSorted(int[] inArr) {
+
+        final int BIT_LENGTH = 8;
+
+
+        // Get the max positive(UINT max) i.e.4294967294 = 2^32 Then divide by 8 to get number of bytes
+        int size = (int)(((long)Integer.MAX_VALUE << 1) / BIT_LENGTH);
+
+        // Then divide by 8 to get number of bytes
+        byte[] bitSet = new byte[size]; // Store 8 bit
+
+        int writeIdx = 0;   // Maintains position to replace dup element with unique one
+
+        for (int i = 0; i < inArr.length; i++) {
+            int posInBitSet = inArr[i] / BIT_LENGTH; // Get index in bitset array
+            int bitPos = inArr[i] % BIT_LENGTH;  // Get bit to turn ON in BitSet array
+
+            if ((bitSet[posInBitSet] & (1 << bitPos)) == 0) {
+                bitSet[posInBitSet] |=  (1 << bitPos);
+                inArr[writeIdx++] = inArr[i];
+            }
+        }
+
+        return Arrays.copyOf(inArr, writeIdx); // Copy original array to new array till unique element position;
     }
 
     //Returns the number of valid entries after deletion
@@ -39,40 +69,5 @@ public class Main {
         }
 
         return Arrays.copyOf(A, writeIdx); // Copy original array to new array till unique element position;
-    }
-
-    // Removes duplicates from UNSORTED array,
-    // Array can have billions of elements
-    // Using byte array to set bit corresponds to the value in original array
-    // BitSet table would look like: b represents byte[]
-    // 4 => b[0]: 0 0 0 1 0 0 0
-    // 3 => b[0]: 0 0 1 0 0 0 0
-    // 15 => b[1]: 0 0 0 0 0 1 0
-    // 21 => b[2]: 0 0 0 0 1 0 0
-    // 88 => b[11]: 0 0 0 0 0 0 1
-    public static int[] deleteDuplicatesFromUnSorted(int[] inArr) {
-
-        final int BIT_LENGTH = 8, MAX = 1 << BIT_LENGTH;
-
-
-        // Get the max positive(UINT max) i.e.4294967294 = 2^32 Then divide by 8 to get number of bytes
-        int size = (int)(((long)Integer.MAX_VALUE << 1) / BIT_LENGTH);
-
-        // Then divide by 8 to get number of bytes
-        byte[] bitSet = new byte[size]; // Store 8 bit
-
-        int writeIdx = 0;   // Maintains position to replace dup element with unique one
-
-        for (int i = 0; i < inArr.length; i++) {
-            int posInBitSet = inArr[i] / BIT_LENGTH; //Get position in BitSet array
-            int bitPos = inArr[i] % BIT_LENGTH;  // Get bit to turn ON in BitSet array
-
-            if ((bitSet[posInBitSet] & (1 << bitPos)) == 0) { // Right-shift 256 till bitPos
-                bitSet[posInBitSet] ^=  (1 << bitPos);
-                inArr[writeIdx++] = inArr[i];
-            }
-        }
-        BitSet bitSet1= new BitSet();
-        return Arrays.copyOf(inArr, writeIdx); // Copy original array to new array till unique element position;
     }
 }
---
+++ SortingProblems/FindMissingNumber/src/com/zavrab/sortingproblems/FindMissingNumberSolution.java
@@ -0,0 +1,113 @@
+package com.zavrab.sortingproblems;
+
+/**
+ * Given an input file with four billion integers, provide an algorithm to generate an integer which is not contained in the file.
+ * Assume you have 1 GB memory. Follow up with what you would do if you have only 10 MB of memory.
+ */
+public class FindMissingNumberSolution {
+
+    public static void main(String[] args) {
+        int[] inArray = new int[] {2, 5, 4, 0, 1};
+        System.out.println("Missing number is: " + findMissingNumber(inArray) + " (Assuming have 1 GB of memory)");
+
+        System.out.println("Missing number is: " + findMissingNumber10MbMemory(inArray) + " (Assuming have only 10 MB of memory)");
+    }
+
+    /**
+     * Find missing number form 4 billion integers, Assume we have only 1GB memory.
+     */
+    public static int findMissingNumber(int[] inArr) {
+        int BIT_LENGTH = 8, totalElements = inArr.length;
+
+        // Get the max positive(UINT max) i.e.4294967294 = 2^32 Then divide by 8 to get number of bytes
+        int size = (int)(((long)Integer.MAX_VALUE << 1) / BIT_LENGTH);
+
+        // Then divide by 8 to get number of bytes
+        byte[] bitSet = new byte[size]; // Store 8 bit
+
+        for (int i = 0; i < totalElements; i++) {
+            int posInBitSet = inArr[i] / BIT_LENGTH; // Get index in bitset array
+            int bitPos = inArr[i] % BIT_LENGTH;  // Get bit to turn ON in BitSet array
+
+            bitSet[posInBitSet] |=  (1 << bitPos);
+        }
+
+        int count = 0;
+        // Iterate through bitset array and find who is missing
+        for (byte i = 0; i < bitSet.length; i++) {
+            for (int bit = 0; bit < BIT_LENGTH; bit++) {
+                if (count++ >= totalElements) { // Assuming: we should only find missing element from given elements i.e. not one next
+                    return Integer.MIN_VALUE;
+                }
+
+                if ((bitSet[i] & (1 << bit)) == 0) {    // Assuming: should return the first missing element
+                    return (i * BIT_LENGTH) + bit;
+                }
+            }
+        }
+        return Integer.MIN_VALUE;
+    }
+
+    /**
+     * Find missing number form 4 billion integers, Assume we have only 10MB memory.
+     */
+    public static int findMissingNumber10MbMemory(int[] inArr) {
+        int BIT_LENGTH = 16, totalElements = inArr.length;
+
+        int size = 1 << BIT_LENGTH;
+
+        int[] bitSet = new int[size]; // Store 32 bit
+
+        // Set 2^16 bits prefix counter
+        for (int i = 0; i < totalElements; i++) {
+            int posInBitSet = inArr[i] / BIT_LENGTH; // Get index in bitset array
+            bitSet[posInBitSet] += 1;   // Increase counter of the bucket
+        }
+
+        // Check which bucket is less than 2^16
+        int bitBucket = findBuketWithLessNumber(bitSet, BIT_LENGTH);
+
+        if (bitBucket < 0) {
+            return  Integer.MIN_VALUE;
+        }
+
+        // Found the prefix, so now set bit for 2^16 suffix
+        BIT_LENGTH = 32;
+        bitSet = new int[size / BIT_LENGTH]; // Reset bitSet to free up old buckets
+        for (int i = 0; i < inArr.length; i++) {
+            int posInBitSet = inArr[i] / BIT_LENGTH; // Get index in bitset array
+            if (posInBitSet == bitBucket) {
+                int bitPos = inArr[i] % BIT_LENGTH;  // Get bit to turn ON in BitSet array
+                bitSet[posInBitSet] |= (1 << bitPos);
+            }
+        }
+
+        return findMissingNumberFromBuckets(bitSet, totalElements, BIT_LENGTH);
+    }
+
+    private static int findMissingNumberFromBuckets(int[] bitArray, int totalElements, int bitLength) {
+        int count = 0;
+        // Iterate through bitset array and find who is missing
+        for (byte i = 0; i < bitArray.length; i++) {
+            for (int bit = 0; bit < bitLength; bit++) {
+                if (count++ >= totalElements) {      // Assuming: we should only find missing element from given elements i.e. not one next
+                    return Integer.MIN_VALUE;
+                }
+
+                if ((bitArray[i] & (1 << bit)) == 0) {    // Assuming: should return the first missing element
+                    return (i * bitLength) + bit;
+                }
+            }
+        }
+        return Integer.MIN_VALUE;
+    }
+
+    private static int findBuketWithLessNumber(int[] bitArray, int bitLength) {
+        for (int i = 0; i < bitArray.length; i++) {
+            if (bitArray[i] < (1 << bitLength)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
