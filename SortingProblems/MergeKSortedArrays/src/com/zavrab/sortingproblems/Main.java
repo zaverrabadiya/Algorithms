bf8b327992c55c414f34c1d7b40765e4d4f06df5
Zaver Rabadiya <zavrab@microsoft.com>
2016-08-14 22:32:04 -0700

    Refactored MergeKSortedArrays solution

--- SortingProblems/MergeKSortedArrays/src/com/zavrab/sortingproblems/Main.java
+++ SortingProblems/MergeKSortedArrays/src/com/zavrab/sortingproblems/Main.java
@@ -1,5 +1,6 @@
 package com.zavrab.sortingproblems;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Main {
@@ -7,8 +8,20 @@ public class Main {
     public static void main(String[] args) {
 	    int[][] input = {
                 {2, 5, 8, 9},
-                {0, 6, 10, 11},
+                {6, 10, 11, 12},
                 {1, 3, 4, 7}
+        };
+
+        int[][] input2 = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}
+        };
+
+        int[][] input3 = {
+                {9, 10, 11, 12},
+                {5, 6, 7, 8},
+                {1, 2, 3, 4}
         };
 
         int[] mergedArray = merge(input, 3, 4);
@@ -20,23 +33,51 @@ public class Main {
 
     public static int[] merge(int[][] arr, int k, int n) {
         int[] output = new int[k * n];
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<Integer>(k);
-        int s = 0;
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<Node>(k);
+        int s = 0, row, col;
+        Node minNode;
 
-        int min;
-        for (int i = 0; i < n; i++) { //Pick ith elements in each array, since they are sorted
-            for (int j = 0; j < k; j++) {
-                priorityQueue.add(arr[j][i]);
-            }
-            min = priorityQueue.poll();
-            output[s++] = min;
+        for (int i = 0; i < k; i++) {
+            priorityQueue.add(new Node(arr[i][0], i, 0));
         }
 
-        while (priorityQueue.size() > 0) {
-            min = priorityQueue.poll();
-            output[s++] = min;
+        while (!priorityQueue.isEmpty()) {
+            minNode = priorityQueue.poll();
+
+            if (minNode.col < n-1) {
+                row = minNode.row;
+                col = minNode.col + 1;
+                priorityQueue.add(new Node(arr[row][col], row, col));
+            }
+
+            output[s++] = minNode.val;
         }
 
         return output;
+    }
+
+    private static class Node implements Comparable {
+        int val;
+        int row;
+        int col;
+
+        Node(int val, int row, int col) {
+            this.val = val;
+            this.row = row;
+            this.col = col;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            Node other = (Node)o;
+
+            if (val < other.val) {
+                return  -1;
+            } else if (val > other.val) {
+                return 1;
+            }
+
+            return 0;
+        }
     }
 }
