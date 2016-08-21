a6358649505108f2f10ce6b8d090fa33a380d2b0
Zaver Rabadiya <zavrab@microsoft.com>
2016-08-20 17:02:36 -0700

    Added different approach for Longest substring without repeating characters

--- StringProblems/LongSubstrWORepChars/src/com/zavrab/stringproblems/Solution.java
+++ StringProblems/LongSubstrWORepChars/src/com/zavrab/stringproblems/Solution.java
@@ -3,14 +3,53 @@ package com.zavrab.stringproblems;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(longestSubstringWithoutRepeatingCharacters("abcabcbb"));
-        System.out.println(longestSubstringWithoutRepeatingCharacters("bbbb"));
-        System.out.println(longestSubstringWithoutRepeatingCharacters("pwwke"));
-        System.out.println(longestSubstringWithoutRepeatingCharacters("pwwkew"));
-        System.out.println(longestSubstringWithoutRepeatingCharacters("abcadefb"));
-        System.out.println(longestSubstringWithoutRepeatingCharacters("abcABC"));
+        System.out.println(longestStringWithoutRepeatingCharacters("abcabcbb"));    // "abc"
+        System.out.println(longestStringWithoutRepeatingCharacters("bbbb"));        // "b"
+        System.out.println(longestStringWithoutRepeatingCharacters("pwwke"));       // "wke"
+        System.out.println(longestStringWithoutRepeatingCharacters("pwwkew"));      // "wke"
+        System.out.println(longestStringWithoutRepeatingCharacters("abcadefb"));    // "bcadef"
+        System.out.println(longestStringWithoutRepeatingCharacters("abcABC"));      // "abc"
     }
 
+    public static String longestStringWithoutRepeatingCharacters(String str) {
+        // Store the index of given character at character's ascii
+        int[] members = getPrePopulatedArray();
+
+        int lenMax = 0, lenSoFar, posStart = 0, posCurr = 0, idxPrev, i = 0;
+        char charCurr;
+
+        for (; i < str.length(); i++) {
+            charCurr = Character.toLowerCase(str.charAt(i));
+            idxPrev = members[charCurr];
+
+            // If same character was found before, then check if that character is still in the range of longest substring seen so far
+            if (idxPrev >= posCurr) {
+                lenSoFar = i - posCurr;
+
+                if (lenSoFar > lenMax) {
+                    lenMax = lenSoFar;
+                    posStart = posCurr;
+                }
+
+                // If same character is in range then move current position to next of that character
+                posCurr = idxPrev + 1;
+            }
+
+            members[charCurr] = i;
+        }
+
+        // If whole string or last substring is unique then if (idxPrev >= posCurr) won't be executed hence max len won't be counted
+        lenSoFar = i - posCurr;
+
+        if (lenSoFar > lenMax) {
+            lenMax = lenSoFar;
+            posStart = posCurr;
+        }
+
+        return str.substring(posStart, (posStart + lenMax));
+    }
+
+    // SOLUTION: 2
     public static String longestSubstringWithoutRepeatingCharacters(String inStr) {
         int[] members = getPrePopulatedArray();
         int subStrStartPos = -1, maxLength = 0, currLength = 0;
