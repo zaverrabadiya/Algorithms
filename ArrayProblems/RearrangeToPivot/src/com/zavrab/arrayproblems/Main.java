0ce15b6a4abc655079264cefdd0365c522efaf27
Zaver Rabadiya <zavrab@microsoft.com>
2016-08-31 23:17:31 -0700

    Cleaned a bit

--- ArrayProblems/RearrangeToPivot/src/com/zavrab/arrayproblems/Main.java
+++ ArrayProblems/RearrangeToPivot/src/com/zavrab/arrayproblems/Main.java
@@ -7,25 +7,29 @@ public class Main {
 
     public static void main(String[] args) {
         int[] a = new int[] {9, 2, 3, 6, 5, 7, 4, 8, 6};
+        String input = "", output = "";
 
-        String input = "";
         for(int v : a) {
             input += v + " ";
         }
+
         System.out.println("Input : " + input);
 
         rearrange(a, 3);
-        String output = "";
+
         for(int v : a) {
             output += v + " ";
         }
+
         System.out.println("Output : " + output);
     }
 
     public static void rearrange(int[] A, int pivotIndex) {
         int i =0, j = 0, l = A.length-1;
         int pivot = A[pivotIndex];
+
         while(j <= l) {
+
             if(A[j] < pivot) {
                 swap(A, i, j);
                 i++;
