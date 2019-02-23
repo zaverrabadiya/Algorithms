301539db8c44a35939f3591effced454b011e3b1
Zaver Rabadiya <zaver.rabadiya@gmail.com>
2019-02-23 15:10:15 -0600

    Adding DP solution

--- DynamicProgramming/PhoneNumbersFormedWithKnightTour/src/com/zavrab/dpproblems/Main.java
+++ DynamicProgramming/PhoneNumbersFormedWithKnightTour/src/com/zavrab/dpproblems/Main.java
@@ -3,16 +3,25 @@ package com.zavrab.dpproblems;
 public class Main {
 
     public static void main(String[] args) {
-        int startDigit = 1, length = 10;
+        int startDigit = 1, length = 3;
+
         System.out.format("Starting number: %d length: %d Total number formed: %d ", startDigit, length, Solution.numPhoneNumbers(startDigit, length));
+        System.out.format("\nStarting number: %d length: %d Total number formed: %d ", startDigit, length, Solution.totalPhoneNumbers(startDigit, length));
+
+        startDigit = 1; length = 10;
+        System.out.format("\n\nStarting number: %d length: %d Total number formed: %d ", startDigit, length, Solution.numPhoneNumbers(startDigit, length));
+        System.out.format("\nStarting number: %d length: %d Total number formed: %d ", startDigit, length, Solution.totalPhoneNumbers(startDigit, length));
 
         startDigit = 6; length = 20;
         System.out.format("\n\nStarting number: %d length: %d Total number formed: %d ", startDigit, length, Solution.numPhoneNumbers(startDigit, length));
+        System.out.format("\nStarting number: %d length: %d Total number formed: %d ", startDigit, length, Solution.totalPhoneNumbers(startDigit, length));
 
         startDigit = 1; length = 5;
         System.out.format("\n\nStarting number: %d length: %d Total number formed: %d ", startDigit, length, Solution.numPhoneNumbers(startDigit, length));
+        System.out.format("\nStarting number: %d length: %d Total number formed: %d ", startDigit, length, Solution.totalPhoneNumbers(startDigit, length));
 
         startDigit = 5; length = 5;
         System.out.format("\n\nStarting number: %d length: %d Total number formed: %d ", startDigit, length, Solution.numPhoneNumbers(startDigit, length));
+        System.out.format("\nStarting number: %d length: %d Total number formed: %d ", startDigit, length, Solution.totalPhoneNumbers(startDigit, length));
     }
 }
--- DynamicProgramming/PhoneNumbersFormedWithKnightTour/src/com/zavrab/dpproblems/Solution.java
+++ DynamicProgramming/PhoneNumbersFormedWithKnightTour/src/com/zavrab/dpproblems/Solution.java
@@ -1,7 +1,10 @@
 package com.zavrab.dpproblems;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Total phone number formed using Knight tour constraint
@@ -23,6 +26,31 @@ public class Solution {
             {-2, -1} // Up-up left
     };
 
+    // DP Solution
+    public static int totalPhoneNumbers(int start, int len) {
+        if (len == 0) return 0;
+        final Map<Integer, List<Integer>> graph = createGraph();
+        int[] cache = new int[10];
+        Arrays.fill(cache, 1);
+
+        for (int l = 2; l <= len; l++) {
+            // Will be used to store sum for current length by looking at previous length's sum
+            int[] currRowSum = new int[10];
+            for (int s = 0; s < 10; s++) {
+
+                // Iterate over each path for current number "s"
+                for (int d : graph.getOrDefault(s, new ArrayList<>(0))) {
+                    currRowSum[s] += cache[d];
+                }
+            }
+
+            cache = currRowSum;
+        }
+
+        return cache[start];
+    }
+
+    // RECURSION with cache
     public static int numPhoneNumbers(int startDigit, int phoneNumberLength) {
         int[][] phonePad = createPhonePad();
         int[][] cache = new int[phoneNumberLength + 1][10];
@@ -95,5 +123,30 @@ public class Solution {
                 {7, 8, 9},
                 {-1, 0, -1}
         };
+    }
+
+    private static Map<Integer, List<Integer>> createGraph() {
+        final Map<Integer, List<Integer>> graph = new HashMap<>();
+        final int[][] pad = createPhonePad();
+
+        for (int i = 0; i < pad.length; i++) {
+            for (int j = 0; j < pad[0].length; j++) {
+
+                for (int[] move : legalMoves) {
+                    int row = i + move[0];
+                    int col = j + move[1];
+
+                    if (isValidMove(pad, row, col)) {
+                        if (!graph.containsKey(pad[i][j])){
+                            graph.put(pad[i][j], new ArrayList<>());
+                        }
+
+                        graph.get(pad[i][j]).add(pad[row][col]);
+                    }
+                }
+            }
+        }
+
+        return graph;
     }
 }
